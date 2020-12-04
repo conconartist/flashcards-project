1be6444767a3f76f6271cff1a8af466777e7d768
conconartist <conconart.info@gmail.com>
2020-12-03 18:13:51 -0700

    Refactor Round test

--- test/Round-test.js
+++ test/Round-test.js
@@ -12,7 +12,7 @@ describe('Round', () => {
 
   beforeEach(() => {
     card1 = new Card(3, 'An object allows you to define a set of related information using what kind of pairs?', ['object-subject', 'key-value', 'index-array'], 'key-value');
-    card2 = new Card(5,'Which dog breed most resembles the data type that indicates a true or false value?', ['Golden Retriver', 'Cocker Spaniel', 'Sheepdog', 'Bolognese'], 'Bolognese');
+    card2 = new Card(5, 'Which dog breed most resembles the data type that indicates a true or false value?', ['Golden Retriver', 'Cocker Spaniel', 'Sheepdog', 'Bolognese'], 'Bolognese');
     card3 = new Card(12, 'What are the two ways you can access properties of an object?', ['bracket notation and dot notation', 'bracket notation and index-array notation', 'dot notation and literal notation'], 'bracket notation and dot notation');
     card4 = new Card(30, 'What is the acronym for primitive data types?', ['BUNNS', 'STUBS', 'BUNY', 'YOLO'], 'BUNNS');
     deck = new Deck([card1, card2, card3, card4]);
